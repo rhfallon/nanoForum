06fd94f9f23c8d9923c7867694115eb3c055d5f0
mitchveazey <mitchveazey@gmail.com>
2020-01-29 10:40:03 -0500

    firebaseui auth

--- nanoforum/src/App.js
+++ nanoforum/src/App.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import index from './components/pages/index'
 
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
 class App extends Component{
   render(){
     return(
--- nanoforum/src/index.js
+++ nanoforum/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
