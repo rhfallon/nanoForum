75b0c9d237781b45c5fe29cb619ebfae60c5dacd
Helen Kim <Helenk490@gmail.com>
2020-01-29 09:45:13 -0500

    path fixed

--- nanoforum/src/App.js
+++ nanoforum/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Index from './Component/Pages/index'
+import index from './components/pages/index'
 
 class App extends Component{
   render(){
     return(
       <Router>
       <div>
-        <Route exact path="/" component= {Index}/>
+        <Route exact path="/" component= {index}/>
       </div>
     </Router>
     )
